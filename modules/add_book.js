7faf08708f47c161bcfae83fd6586366afc81f9c
AndreaMOr <andreamanuel2207@gmail.com>
2023-06-05 13:28:29 -0600

    Create module to add a new book

---
+++ modules/add_book.js
@@ -0,0 +1,17 @@
+/* eslint-disable import/prefer-default-export */
+export const addBook = (booklist, save, load) => {
+  const bookTitle = document.querySelector('#book-title');
+  const bookAuthor = document.querySelector('#book-author');
+
+  const book = {
+    bookTitle: bookTitle.value,
+    bookAuthor: bookAuthor.value,
+  };
+
+  booklist.push(book);
+  save();
+  load();
+
+  bookTitle.value = '';
+  bookAuthor.value = '';
+};
